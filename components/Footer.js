e4f43c176edf49ae1e3a7d4555c1f4f75a788bc9
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-01-23 10:58:11 -0500

    added a fixed position

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function Header() {
+export default function Footer() {
   return (
     <View style={styles.footerContainer}>
       <Text style={styles.footerText}>All rights reserved by Little Lemon, 2021</Text>
@@ -10,11 +10,13 @@ export default function Header() {
 
 const styles = StyleSheet.create({
   footerContainer: {
-    flex: 0.2,
-    backgroundColor: '#F4CE14'
+    backgroundColor: '#F4CE14',
+    position: 'fixed',
+    bottom: 0,
   },
-  headerText: { 
+  footerText: { 
    padding: 15,
-   fontSize: '15',
+   fontSize: 10,
+   textAlign: 'center',
   },
 });
