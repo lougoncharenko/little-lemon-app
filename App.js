400f67fe362998602dfd1d7bb6febd7c42110222
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-01-23 10:58:23 -0500

    added a content section

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 //components
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Header />
+     <View style={styles.content}>
+       <Header />
+      </View> 
+      <Footer />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +20,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#495E57',
-    padding: 45,
+  },
+  content: {
+    flex: 1
   },
 });
