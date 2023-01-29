19c645afdfd831585dd64e68c56aa13aa8f0b50d
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-01-29 17:25:21 -0500

    displayed menu prices

--- components/MenuItems.js
+++ components/MenuItems.js
@@ -7,22 +7,22 @@ const menuItemsToDisplay = [
     { name: 'Moutabal', price: '$5.00', id: '2B' },
     { name: 'Falafel', price: '$7.50', id: '3C' },
     { name: 'Marinated Olives', price: '$5.00', id: '4D' },
-    { name: 'Kofta', price: '$5.00', id: '5E' },
-    { name: 'Eggplant Salad', price: '$8.50', id: '6F' },
-    { name: 'Lentil Burger', price: '$10.00', id: '7G' },
-    { name: 'Smoked Salmon', price: '$14.00', id: '8H' },
-    { name: 'Kofta Burger', price: '$11.00', id: '9I' },
+    { name: 'Kofta', price: '$6.00', id: '5E' },
+    { name: 'Eggplant Salad', price: '$9.50', id: '6F' },
+    { name: 'Lentil Burger', price: '$12.50', id: '7G' },
+    { name: 'Smoked Salmon', price: '$18.00', id: '8H' },
+    { name: 'Kofta Burger', price: '$12.50', id: '9I' },
     { name: 'Turkish Kebab', price: '$15.50', id: '10J' },
-    { name: 'Fries', price: '$6.00', id: '11K' },
+    { name: 'Fries', price: '$8.00', id: '11K' },
     { name: 'Buttered Rice', price: '$4.50', id: '12L' },
     { name: 'Bread Sticks', price: '$5.50', id: '13M' },
     { name: 'Pita Pocket', price: '$3.00', id: '14N' },
     { name: 'Lentil Soup', price: '$6.75', id: '15O' },
-    { name: 'Greek Salad', price: '$8.00', id: '16Q' },
+    { name: 'Greek Salad', price: '$8.50', id: '16Q' },
     { name: 'Rice Pilaf', price: '$7.00', id: '17R' },
     { name: 'Baklava', price: '$5.00', id: '18S' },
     { name: 'Tartufo', price: '$5.00', id: '19T' },
-    { name: 'Tiramisu', price: '$7.00', id: '20U' },
+    { name: 'Tiramisu', price: '$6.00', id: '20U' },
     { name: 'Panna Cotta', price: '$5.00', id: '21V' },
   ];
 
@@ -33,28 +33,8 @@ export default function MenuItems() {
             <Text style={styles.menuText}>Menu Items</Text>
         </View>
         <FlatList
-          data={[
-            {key: 'Hummus'},
-            {key: 'Moutabal'},
-            {key: 'Falafel'},
-            {key: 'Marinated Olives'},
-            {key: 'Kofta'},
-            {key: 'Eggplant Salad'},
-            {key: 'Lentil Burger'},
-            {key: 'Smoked Salmon'},
-            {key: 'Kofta Burger'},
-            {key: 'Turkish Kebab'},
-            {key: 'Buttered Rice'},
-            {key: 'Breadsticks'},
-            {key: 'Gyro'},
-            {key: 'Lentil Soup'},
-            {key: 'Greek Salad'},
-            {key: 'Baklava'},
-            {key: 'Rice Pilaf'},
-            {key: 'Tiramisu'},
-            {key: 'Panna Cotta'},
-          ]}
-          renderItem={({item}) => <Text style={styles.menuItem}>{item.key}</Text>}
+          data={menuItemsToDisplay}
+          renderItem={({item}) => <Text style={styles.menuItem}>{item.name}:  {item.price} </Text>}
         />
       </View>
     );
@@ -66,13 +46,14 @@ const styles = StyleSheet.create({
       paddingTop: 22,
     },
     menuHeader: {
+        paddingBottom: 30,
     },
     menuText: {
      textAlign: 'center',
      fontSize: 25,
      paddingHorizontal: 20, 
      color: '#EDEFEE',
-
+     textDecorationLine: 'underline',
     },
     menuItem: {
       padding: 10,
@@ -81,6 +62,7 @@ const styles = StyleSheet.create({
       color: '#EDEFEE',
       paddingLeft: 40,
       paddingHorizontal: 15,
+      display: 'flex'
     },
   });
   
